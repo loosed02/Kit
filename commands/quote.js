2331a33564fc3649beeec2b7946d29d69c559fb4
Max W <39675015+Kaboodle3005@users.noreply.github.com>
2018-08-28 14:17:54 -0700

    Added a quote command, help coming soon

---
+++ commands/quote.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+
+    async function fetchM(id, ch){
+
+        if(!id) return;
+
+        ch = ch.replace("<#", "").replace(">", "");
+        var chan = message.guild.channels.find('id', ch);
+        var mVar = await chan.fetchMessage(id);
+    
+        if(!mVar){
+            //no message found
+            const embed = new Discord.RichEmbed()
+            .setColor(0xF46242)
+            .setDescription("No message found")
+            return message.channel.send({embed});
+        } else {
+            const embed = new Discord.RichEmbed()
+            .setAuthor(mVar.author.username, mVar.author.avatarURL)
+            .setDescription(mVar.content)
+            return message.channel.send({embed});
+        }
+
+    }
+
+    if(!args[1]){
+        var firstArg = args[0];
+        var secondArg = message.channel.id;
+    } else {
+        var firstArg = args[0];
+        var secondArg = args[1];
+    }
+
+    fetchM(firstArg, secondArg);
+
+}
