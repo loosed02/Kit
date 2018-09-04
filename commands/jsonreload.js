d5c14559f1ff84d59ebfe8ddbf1ed040f7054df8
Max W <39675015+Kaboodle3005@users.noreply.github.com>
2018-09-04 10:32:35 -0700

    Added JSON hotloader

---
+++ commands/jsonreload.js
@@ -0,0 +1,23 @@
+const config = require("./../config.json");
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+  if(!message.author.id === config.owner && !message.author.id === "454461184792461312"){
+    return;
+  } else {
+
+    if(!args || args.size < 1) return;
+    delete require.cache[require.resolve(`./../JSON/${args[0]}.json`)];
+
+    let embedVar = new Discord.RichEmbed()
+           .setTimestamp()
+           .setDescription(`The JSON \`${args[0]}.json\` has been reloaded`)
+           message.channel.send({embed: embedVar});
+
+  }
+  }
+  
+  exports.conf = {
+    DM: true,
+    OwnerOnly: false
+}
