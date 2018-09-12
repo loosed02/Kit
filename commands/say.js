9f9a5dc441605dd244cad38097270e2c62a598b1
Max W <39675015+Kaboodle3005@users.noreply.github.com>
2018-09-11 20:22:25 -0700

    Bug fixes

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 
 exports.run = (client, message, args, deletedMessage, talkedRecently, embeddedRecently, weatheredRecently, commandCount, coinsSet, roles, queue, sql) => {
 if(message.author.id === "378769654942007299"){ 
-	message.delete().catch(O_o=>{});
+	//message.delete().catch(O_o=>{});
 
 	var chan = client.channels.find('id', args[0]);
 	chan.send(args.slice(1).join(' '));
