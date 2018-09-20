7a3498b6f64869561498c8d26dc76ece810d63f6
Max W <39675015+Kaboodle3005@users.noreply.github.com>
2018-09-20 10:49:20 -0700

    Typo fixed in Help

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
     if(!args[0]){
         const embed = new Discord.RichEmbed()
         .setTitle("Module List - <> = Disabled")
-        .setDescription("Use `@kit checkprefix` to see this server's prefix\nView documentation [Here](https://kitk.us/)\n")
+        .setDescription("Use `@kit checkprefix` to see this server's prefix\nView site [Here](https://bot.kitk.us/)\n")
         .addField("Staff Commands", "```Ban, Kick, Purge, Prefix, Snipe, RoleBan, SlowMode```")
         .addField("Utility Commands", "```Quote, Help, Avatar, Info, Youtube, ReportBug, Temp, Welcome, NonASCII, Imgur, Profile, Coins, Note```")
         .addField("Searching Commands", "```Weather, Bird, Cat, Rabbit```")
