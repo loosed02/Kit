9b234945c4468f8d07cab846cc20a039752fd0be
Max W <39675015+Kaboodle3005@users.noreply.github.com>
2018-09-22 11:51:03 -0700

    Added some jokes

---
+++ commands/bongocat.js
@@ -0,0 +1,10 @@
+//<a:bongocat:490193507009429505> 
+
+exports.run = (client, message, args) => {
+    message.channel.send('<a:bongocat:490193507009429505>').then().catch(console.error);
+    }
+    
+    exports.conf = {
+        DM: true,
+        OwnerOnly: false
+    }
