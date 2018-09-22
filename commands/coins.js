0b1242f2182021cc6cbea09af2ebb1a447bdcc4e
Max W <39675015+Kaboodle3005@users.noreply.github.com>
2018-09-22 11:51:18 -0700

    Prototype coins leaderboard

--- commands/coins.js
+++ commands/coins.js
@@ -18,54 +18,95 @@ exports.run = (client, message, args, deletedMessage, talkedRecently, embeddedRe
     var IDarray = donorIDArray;
 
 	if(args[0] === "add"){
-		//trigger 24hr cooldown
-		if (coinsSet.has(message.author.id)) {
-			return message.reply("There is a 24 hour cooldown on this command, try again later");
-		} else {
-		coinsSet.add(message.author.id);
-		setTimeout(() => {
-		  coinsSet.delete(message.author.id);
-		}, 86400000);
-		//execute module here
-		sql.get(`SELECT quarters FROM profile WHERE userId ="${message.author.id}"`).then(row => {
+		sql.get(`SELECT * FROM profile WHERE userId ="${message.author.id}"`).then(row => {
+		
 			if(!row){
-				//make profile first
 				const embed = new Discord.RichEmbed()
 					.setColor(0xF46242)
 					.setTimestamp()
 					.setDescription("Create a profile first")
-					.setFooter("Use `k?profile`")
+					.setFooter("Use k?profile create")
 					message.channel.send({embed});
-
 			} else {
-				if(donorIDArray.includes(message.author.id)){
-					sql.run(`UPDATE profile SET quarers = "${parseInt(row.quarters) + 4}" WHERE userId = "${message.author.id}"`);
-		const embed = new Discord.RichEmbed()
+		
+		if(message.createdTimestamp > row.qTime || !row.qTime){
 
-				.setTimestamp()
-				.setDescription("You got 4 quarters")
+				if(donorIDArray.includes(message.author.id)){
+					sql.run(`UPDATE profile SET quarters = "${parseInt(row.quarters) + 4}" WHERE userId = "${message.author.id}"`);
+					sql.run(`UPDATE profile SET qTime = "${message.createdTimestamp + 24*60*60*1000}" WHERE userId = "${message.author.id}"`);
+
+				const embed = new Discord.RichEmbed()
+
+					.setDescription("You got 4 quarters")
+					.setFooter(row.qTime)
 				message.channel.send({embed});
 				} else {
-		sql.run(`UPDATE profile SET quarters = "${parseInt(row.quarters) + 1}" WHERE userId = "${message.author.id}"`);
-		const embed = new Discord.RichEmbed()
+					sql.run(`UPDATE profile SET quarters = "${parseInt(row.quarters) + 1}" WHERE userId = "${message.author.id}"`);
+					sql.run(`UPDATE profile SET qTime = "${message.createdTimestamp + 24*60*60*1000}" WHERE userId = "${message.author.id}"`);
 
-				.setTimestamp()
-				.setDescription("You got a quarter")
+				const embed = new Discord.RichEmbed()
+
+					.setTimestamp()
+					.setDescription("You got a quarter")
 				message.channel.send({embed});
 				}
-				}
-			//you got one point
-			
 
-		}).catch((err) => {
-					const embed = new Discord.RichEmbed()
+			} else {
+				return message.reply("There is a 24 hour cooldown on this command, try again later");
+			}
+	  }
+	});
+    } else if(args[0] === "raw"){
+
+		sql.get(`SELECT * FROM profile WHERE userId ="${message.author.id}"`).then(row => {
+
+				const embed = new Discord.RichEmbed()
 					.setColor(0xF46242)
-					.setTimestamp()
-					.setDescription("An error occured")
-					.setFooter(err)
-					message.channel.send({embed});
+					.addField('userId', row.userId)
+					.addField('quarters', row.quarters)
+					.addField('badge', row.badge)
+					.addField('desc', row.desc)
+					.addField('color', row.color)
+					.addField('cmds', row.cmds)
+					.addField('bg', row.bg)
+					.addField('qTime', row.qTime)
+				message.channel.send({embed});
+
 		});
-		}
+
+	} else if(args[0] === "leaderboard"){
+
+		sql.all(`SELECT * FROM profile`).then(row => {
+
+			//console.log(row);
+			//console.log(row.length);
+
+			var i = 1;
+			var ar = [];
+			while(i < row.length){
+				ar[i-1] = {
+					userId: row[i-1].userId,
+					quarters: parseInt(row[i-1].quarters)
+				};
+				i = i + 1;
+			}
+
+
+				ar = ar.sort(function(a, b) {
+					return parseFloat(b.quarters) - parseFloat(a.quarters);
+				});
+
+			const embed = new Discord.RichEmbed()
+					.setDescription('```' + 
+						"\n" + ar[0].userId + " - " + ar[0].quarters +
+						"\n" + ar[1].userId + " - " + ar[1].quarters +
+						"\n" + ar[2].userId + " - " + ar[2].quarters +
+						"\n" + ar[3].userId + " - " + ar[3].quarters +
+						"\n" + ar[4].userId + " - " + ar[4].quarters +
+			        + '```')
+			message.channel.send({embed});
+
+		});
 
 	} else if(args[0] === "get"){
 		//search for user
@@ -111,25 +152,24 @@ exports.run = (client, message, args, deletedMessage, talkedRecently, embeddedRe
 					message.channel.send({embed});
 
 			} else {
-				if (coinsSet.has(message.author.id)) {
-		//sql.run(`UPDATE profiles SET quarters = "${row.quarters + 1}" WHERE userId = "${message.author.id}"`);
-		const embed = new Discord.RichEmbed()
+				sql.get(`SELECT * FROM profile WHERE userId ="${message.author.id}"`).then(row => {
+				if (message.createdTimestamp > row.qTime || !row.qTime) {
+					const embed = new Discord.RichEmbed()
+
+						.setTimestamp()
+						.setColor(0x32ff58)
+						.setDescription("You have " + row.quarters + " quarters")
+					message.channel.send({embed});
+			} else {
+				const embed = new Discord.RichEmbed()
 
 				.setTimestamp()
 				.setColor(0xF46242)
 				.setDescription("You have " + row.quarters + " quarters")
 				message.channel.send({embed});
-				
-			} else {
-				const embed = new Discord.RichEmbed()
-
-				.setTimestamp()
-				.setColor(0x32ff58)
-				.setDescription("You have " + row.quarters + " quarters")
-				message.channel.send({embed});
 			}
 			//you got one point
-			
+			});
 		}
 		});
 
@@ -138,6 +178,6 @@ exports.run = (client, message, args, deletedMessage, talkedRecently, embeddedRe
 }
 
 exports.conf = {
-    DM:  true,
+    DM: true,
     OwnerOnly: false
 }
